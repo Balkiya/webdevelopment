b47868af0d18268c056af09f711defdd2307471f
Balkiya <balkiyakumisbaevaa.91@gmail.com>
2024-02-25 13:23:09 +0600

    database1

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    publisher = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+    isbn = models.CharField(max_length=13)
+    genre = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    cover = models.ImageField(upload_to='book_covers/')
+
+    def __str__(self):
+        return self.title
+
+
+class BookInstance(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    status = models.CharField(max_length=50)
+    acquisition_date = models.DateField()
+    last_updated_date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.book.title} - {self.status}'
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=50)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Order {self.id} by {self.user.username}'
+
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    review_text = models.TextField()
+    rating = models.IntegerField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Review for {self.book.title} by {self.user.username}'
